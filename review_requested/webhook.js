b372481fe03c6a1e3482d37d30965795753a2b99
Armand Fardeau <fardeauarmand@gmail.com>
2021-02-18 17:59:44 +0100

    Add webhook

---
+++ review_requested/webhook.js
@@ -0,0 +1,58 @@
+const githubUsers = {
+    "armandfardeau": "@armandfardeau",
+    "Quentinchampenois": "@quentin",
+    "moustachu": "@moustachu",
+    "Dynnammo": "@baptiste",
+    "MoretS": "@seb"
+};
+
+const GithubUsersToRocketUsers = (login) => {
+    if (githubUsers.hasOwnProperty(login)) {
+        return githubUsers[login];
+    } else {
+        return login;
+    }
+};
+
+const users = (obj) => {
+    const reviewers = obj.pull_request.requested_reviewers.map((elem) => {
+        return GithubUsersToRocketUsers(elem.login);
+    });
+
+    if (reviewers.length > 1) {
+        return reviewers.join(" / ");
+    } else {
+        return reviewers[0];
+    }
+};
+
+const buildMessage = (obj) => {
+    const pr = obj.pull_request.html_url;
+    const reviewers = users(obj);
+
+    let template = `[A review has been requested](${pr}) for ${reviewers}`;
+
+    return {
+        text: template
+    };
+};
+
+/* exported Script */
+class Script {
+    /**
+     * @params {object} request
+     */
+    process_incoming_request({request}) {
+        if (request.content.action === "review_requested") {
+            return {
+                content: {
+                    attachments: [{
+                        text: buildMessage(request.content).text
+                    }]
+                }
+            };
+        } else {
+            return;
+        }
+    }
+}
